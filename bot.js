72eb271354725d418ac6133e8046611fa8e4edff
tknow85 <tknow85@gmail.com>
2017-12-16 00:08:20 +0100

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,26 +1,33 @@
 const Discord = require('discord.js');
 const client = new Discord.Client();
+const prefix = '!';
 
 client.on('ready', () => {
     console.log('I am ready!');
 });
 
 client.on('message', message => {
-    if (message.content === 'ping') {
-        message.reply('pong');
+    if (!message.content.startsWith(prefix) || message.author.bot) return;
+    const msgContent = message.content.split(prefix).slice(1);
+    let msg = false;
+    switch(msgContent) {
+        case 'ping':
+            msg = 'pong';
+        break;
+        case 'avatar':
+            msg = message.author.avatarURL;
+        break;
     }
-});
 
-client.on('message', message => {
-  if (message.content === 'avatar') {
-    message.reply(message.author.avatarURL);
-  }
+    if(msg) {
+        message.reply(msg);
+    }  
 });
 
 client.on('guildMemberAdd', member => {
-  const channel = member.guild.channels.find('name', 'general');
-  if (!channel) return;
-  channel.send(`Welcome to the server, ${member}`);
+    const channel = member.guild.channels.find('name', 'general');
+    if (!channel) return;
+    channel.send(`Welcome to the server, ${member}`);
 });
 
 client.login(process.env.BOT_TOKEN);
