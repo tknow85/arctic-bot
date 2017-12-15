e6eab5ddec844bbaa605e7d1bf41b4dca6c3118b
tknow85 <tknow85@gmail.com>
2017-12-15 23:17:54 +0100

    Update bot.js

--- bot.js
+++ bot.js
@@ -11,4 +11,18 @@ client.on('message', message => {
     }
 });
 
+client.on('message', message => {
+  // If the message is "what is my avatar"
+  if (message.content === 'avatar') {
+    // Send the user's avatar URL
+    message.reply(message.author.avatarURL);
+  }
+});
+
+client.on('guildMemberAdd', member => {
+  const channel = member.guild.channels.find('name', 'member-log');
+  if (!channel) return;
+  channel.send(`Welcome to the server, ${member}`);
+});
+
 client.login(process.env.BOT_TOKEN);
